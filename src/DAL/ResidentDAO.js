535fca63d27b0d2e698c1d61dcaa957253ebab9e
benjamin <benjamin.biollaz@students.hevs.ch>
2023-02-27 11:02:02 +0100

    Firebase first access

---
+++ src/DAL/ResidentDAO.js
@@ -0,0 +1,13 @@
+import {doc, getDoc, setDoc, updateDoc} from "firebase/firestore";
+import { db } from "./FirebaseConf";
+import { residentConverter } from "../DTO/ResidentDTO";
+
+class ResidentDAO {
+
+    async getresidentById(residentId) {
+        const r = await getDoc(doc(db, "Residents", residentId).withConverter(residentConverter));
+        return r.data();
+    }
+}
+
+export {ResidentDAO};
