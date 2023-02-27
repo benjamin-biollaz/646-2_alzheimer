9f921c3f0c1c04bfc3bf2965f96d51973f032abc
benjamin <benjamin.biollaz@students.hevs.ch>
2023-02-27 14:48:14 +0100

    db access in app.js

--- src/Presentation/Components/App.js
+++ src/Presentation/Components/App.js
@@ -1,12 +1,26 @@
 import '../CSS/App.css';
+import React, {useEffect} from "react";
 import Home from './Home.js';
+import { ResidentDAO } from "../../DAL/ResidentDAO";
+import { ResidentDTO } from "../../DTO/ResidentDTO";
 
 function App() {
+  const [resident, setResident] = React.useState(null);
+
+    useEffect(() => {
+        getResident();
+    }, []);
+
   return (
     <div className="App">
-      <Home/>
+      <Home resident={resident}/>
     </div>
   );
+
+  async function getResident() {
+    const res = await ResidentDAO.prototype.getresidentById("HvrELV7MRnnJcV24ro1w");
+    setResident(res);
+}
 }
 
 export default App;
