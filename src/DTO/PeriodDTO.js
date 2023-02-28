5628365ed4f638f45ba95868dda0325113c08f08
benjamin <benjamin.biollaz@students.hevs.ch>
2023-02-28 14:41:20 +0100

    timeline DB access

---
+++ src/DTO/PeriodDTO.js
@@ -0,0 +1,30 @@
+class PeriodDTO {
+    constructor(name, location, startDate, endDate) {
+        this.name = name;
+        this.location = location;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    toString() {
+        return this.name + " " + this.location + " " + this.startDate + " " + this.endDate;
+    }
+}
+
+const periodConverter = {
+    toFirestore(period) {
+        return {
+            name: period.name, location: period.location, 
+            startDate: period.startDate, endDate: period.endDate
+        };
+    },
+    fromFirestore(
+        snapshot,
+        options
+    ) {
+        const data = snapshot.data(options);
+        return new PeriodDTO(data.name, data.location, data.startDate, data.endDate);
+    }
+};
+export {periodConverter};
+export {PeriodDTO}
