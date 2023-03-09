5c6e2d973ef9562d1640d87f2868eae81e42cc9d
benjamin <benjamin.biollaz@students.hevs.ch>
2023-03-09 13:58:41 +0100

    Event update

--- src/DTO/EventWithId.js
+++ src/DTO/EventWithId.js
@@ -1,3 +1,7 @@
+/**
+ * EventWithId contains both the document id and the DTO object 
+ * that can be sent to the database.
+ */
 class EventWithId {
     constructor (id, eventDTO) {
         this.id = id;
