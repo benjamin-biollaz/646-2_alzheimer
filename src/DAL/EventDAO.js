db889df8b39fc1ed625a608a050cda9d211cd8ca
benjamin <benjamin.biollaz@students.hevs.ch>
2023-03-09 13:58:41 +0100

    Event update

--- src/DAL/EventDAO.js
+++ src/DAL/EventDAO.js
@@ -1,9 +1,15 @@
-import { doc, getDocs, collection, withConverter } from "firebase/firestore";
+import { doc, getDocs, collection, withConverter, updateDoc, setDoc } from "firebase/firestore";
 import { db } from "./FirebaseConf";
-import { eventConverter } from "../DTO/EventDTO";
+import { eventConverter, EventDTO } from "../DTO/EventDTO";
 import { EventWithId } from "../DTO/EventWithId";
 
 class EventDAO {
+
+    /**
+     * Get all the events of a timeline.
+     * @param {The timeline id} timelineId 
+     * @returns A list of "EventWithId" type.
+     */
     async getEventsByTimelineId(timelineId) {
         // get the events related to the timeline
         const eventsRef =
@@ -17,10 +23,28 @@ class EventDAO {
         return docSnapshot.docs.map(e => new EventWithId(e.id, e.data()));
     }
 
-    async updateEventBy(timelineId, eventId, date, name) {
-        
-    }
+    /**
+     * Update the event
+     * @param {*} timelineId 
+     * @param {The old event, must be of type EventWithId.} eventToChange 
+     * @param {New date} date 
+     * @param {New name} name 
+     * @returns 
+     */
+    async updateEvent(timelineId, eventToChange, date, name) {
+        // access DB only if changes have been made 
+        if (eventToChange.date == date && eventToChange.name == name)
+            return;
 
+            const eventRef =
+            doc(
+            collection(
+                doc(collection(db, "Timelines"),timelineId),
+                "Events")
+                , eventToChange.id).withConverter(eventConverter);
+            
+            await setDoc(eventRef, new EventDTO(date, name));
+    }
    
 }
 
