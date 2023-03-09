d5ce703567a2075dee28cfe3d026be971b4fa6e9
benjamin <benjamin.biollaz@students.hevs.ch>
2023-03-09 14:36:13 +0100

    Database access for period

--- src/DAL/PeriodDAO.js
+++ src/DAL/PeriodDAO.js
@@ -1,8 +1,13 @@
-import { doc, getDocs, collection, withConverter } from "firebase/firestore";
+import { doc, getDocs, collection, withConverter, setDoc } from "firebase/firestore";
 import { db } from "./FirebaseConf";
-import { periodConverter } from "../DTO/PeriodDTO";
+import { periodConverter, PeriodDTO } from "../DTO/PeriodDTO";
 
 class PeriodDAO {
+    /**
+     * Get all the periods of one timeline.
+     * @param {The timeline document id} timelineId 
+     * @returns A list of "TimelineWithId" instances 
+     */
     async getPeriodsByTimelineId(timelineId) {
         // get the periods related to the timeline
         const periodsRef = collection(
@@ -13,6 +18,31 @@ class PeriodDAO {
 
         const docSnapshot = await getDocs(periodsRef);
         return docSnapshot.docs.map(e => e.data());
+    }
+
+    /**
+     * Update the period in the database.
+     * @param {The timeline document id} timelineId 
+     * @param {The old period object} periodToChange 
+     * @param {The new start date} newStartDate 
+     * @param {The new end date} newEndDate 
+     * @param {The new name} newName 
+     * @returns nothing
+     */
+    async updatePeriod(timelineId, periodToChange, newStartDate, newEndDate, newName) {
+        // access DB only if changes have been made 
+        if (periodToChange.startDate == newStartDate && periodToChange.endDate == newEndDate
+            && periodToChange.name == newName)
+            return;
+
+        const eventRef =
+            doc(
+                collection(
+                    doc(collection(db, "Timelines"), timelineId),
+                    "Periods")
+                , periodToChange.id).withConverter(periodConverter);
+
+        await setDoc(eventRef, new PeriodDTO(newName, newStartDate, newEndDate));
     }
 }
 
