1221226bb9160d262980513901ccff4acc9ac652
Tatu Paukkeri <tatu.paukkeri@gmail.com>
2023-03-10 11:42:56 +0200

    resident birthdate and age

--- src/Presentation/Components/Home.js
+++ src/Presentation/Components/Home.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { DateFormatter } from "../../Utilities/DateFormatter";
 
 export default function Home({ resident }) {
-  return (
-    <div>
-      <h1 data-testid="1">Hello World!</h1>
-      <h2 data-testid="2">{resident?.firstName}</h2>
-    </div>
-  );
+
+    const formatter = new DateFormatter();
+    const residentAge = formatter.calculateAge(resident?.birthDate)
+    const birthDate = formatter.formatDate(resident?.birthDate)
+    
+    return (
+        <div>
+            <h1 data-testid="1">Hello World!</h1>
+            <h2 data-testid="2">{resident?.firstName} {resident?.lastName}, {residentAge}</h2>
+            <h2>{birthDate}</h2>
+        </div>
+    );
+
 }
+
