c8f419cdf758e10b24d45da9ff6e32c43b4f566a
etdnp <etdn.prod@gmail.com>
2023-03-14 10:13:30 +0100

    Information page skeleton done

--- src/Presentation/Components/Information.js
+++ src/Presentation/Components/Information.js
@@ -3,6 +3,17 @@ import Navbar from "./Navbar";
 import "../CSS/Information.css";
 import "../fonts/LexendDeca.ttf";
 import { GiMusicalNotes } from "react-icons/gi";
+import { GiFullPizza } from "react-icons/gi";
+import { GiNoodles } from "react-icons/gi";
+import { TbCarrotOff } from "react-icons/tb";
+import { TbPizzaOff } from "react-icons/tb";
+import { TbFishOff } from "react-icons/tb";
+import { FaBath } from "react-icons/fa";
+import { GiShower } from "react-icons/gi";
+import { GiNightSleep } from "react-icons/gi";
+import { BsFillSunriseFill, BsSunriseFill } from "react-icons/bs";
+import { TfiTimer } from "react-icons/tfi";
+import { GiMeal } from "react-icons/gi";
 
 function Information() {
   function MusicNoteIcon() {
@@ -23,22 +34,22 @@ function Information() {
       <div className="container_infos">
         <div className="passions">
           <h3 className="label">Passions</h3>
-          <div className="passion_list">
-            <span className="passion_item">
+          <div className="infos_list">
+            <span className="infos_item">
               Musique
               <div className="icon">
                 <GiMusicalNotes />
               </div>
             </span>
             &nbsp; &nbsp; &nbsp;
-            <span className="passion_item">
+            <span className="infos_item">
               Poterie
               <div className="icon">
                 <GiMusicalNotes />
               </div>
             </span>
             &nbsp; &nbsp; &nbsp;
-            <span className="passion_item">
+            <span className="infos_item">
               Tricot
               <div className="icon">
                 <GiMusicalNotes />
@@ -48,15 +59,139 @@ function Information() {
         </div>
         <div className="preferences">
           <h3 className="label">Préférences</h3>
+
+          <div className="infos_list">
+            <h4 className="categories">Alimentation</h4>
+            <span className="infos_item">
+              <div className="icon">
+                <GiFullPizza />
+              </div>
+            </span>
+            &nbsp; &nbsp;
+            <span className="infos_item">
+              <div className="icon">
+                <GiNoodles />
+              </div>
+            </span>
+            &nbsp; &nbsp;
+            <span className="infos_item">
+              <div className="icon">
+                <GiNoodles />
+              </div>
+            </span>
+            &nbsp; &nbsp;
+            <span className="infos_item">
+              <div className="icon">
+                <TbCarrotOff />
+              </div>
+            </span>
+            &nbsp; &nbsp; &nbsp;
+            <span className="infos_item">
+              <div className="icon">
+                <TbPizzaOff />
+              </div>
+            </span>
+            &nbsp; &nbsp;
+            <span className="infos_item">
+              <div className="icon">
+                <TbFishOff />
+              </div>
+            </span>
+          </div>
+
+          <div className="infos_list">
+            {" "}
+            <h4 className="categories">Hygiène</h4>
+            <span className="infos_item">
+              <div className="icon">
+                <FaBath />
+              </div>
+            </span>
+            &nbsp; &nbsp; &nbsp;
+            <span className="infos_item">
+              <div className="icon">
+                <GiShower />
+              </div>
+            </span>
+          </div>
         </div>
+        {/* //---------------- EVENTS ------------------// */}
         <div className="evenements">
           <h3 className="label">Évènements</h3>
         </div>
+
+        {/* //---------------- HABITUDES ------------------// */}
         <div className="habitudes">
           <h3 className="label">Habitudes</h3>
+          <div className="habitudes_grid">
+            <div className="infos_list">
+              {" "}
+              <h4 className="categories_1">Lever</h4>
+              <span className="infos_item">
+                <div className="icon">
+                  <BsSunriseFill />
+                </div>
+              </span>
+            </div>
+
+            <div className="infos_list">
+              {" "}
+              <h4 className="categories_2">Coucher</h4>
+              <span className="infos_item">
+                <div className="icon">
+                  <GiNightSleep />
+                </div>
+              </span>
+            </div>
+
+            <div className="infos_list">
+              {" "}
+              <h4 className="categories_3">Souper</h4>
+              <span className="infos_item">
+                <div className="icon">
+                  <GiMeal />
+                </div>
+              </span>
+            </div>
+
+            <div className="infos_list">
+              {" "}
+              <h4 className="categories_4">Sieste</h4>
+              <span className="infos_item">
+                <div className="icon">
+                  <TfiTimer />
+                </div>
+              </span>
+            </div>
+          </div>
         </div>
+
         <div className="croyances">
           <h3 className="label">Croyances</h3>
+
+          <div className="infos_list">
+            <h4 className="categories">Religion</h4>
+            <span className="infos_religion">
+              <h3>Christianisme</h3>
+            </span>
+            &nbsp; &nbsp;
+          </div>
+
+          <div className="infos_list">
+            <h4 className="categories">Pratique</h4>
+            <span className="infos_religion">
+              <h3>Prière</h3>
+            </span>
+            &nbsp; &nbsp;
+          </div>
+
+          <div className="infos_list">
+            <h4 className="categories">Valeurs</h4>
+            <span className="infos_religion">
+              <h3>Respect</h3>
+            </span>
+            &nbsp; &nbsp;
+          </div>
         </div>
       </div>
     </>
