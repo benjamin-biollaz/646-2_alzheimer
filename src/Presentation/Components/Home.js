9c0c3bd1e0a46c2b2dbad563faf979249b742828
Benjamin Morel <benjamin.morel@students.hevs.ch>
2023-03-14 15:37:23 +0100

    Update home page

--- src/Presentation/Components/Home.js
+++ src/Presentation/Components/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { TimelineWidget } from './Timeline/Timeline';
 import "../CSS/Home.css"
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Information from "./Information";
+import { Link } from 'react-router-dom';
 
 export default function Home({ resident }) {
 
   return (
-    <div>
-      <h1 data-testid="1">Hello World!</h1>
-      <h2 data-testid="2">{resident?.firstName}</h2>
+    <div style={{ marginLeft: "10%", marginTop: "5%" }}>
+      <h1 data-testid="1">Hello {resident?.firstName}</h1>
+      <Link to="/infos">Information page</Link>
     </div>
   );
 
