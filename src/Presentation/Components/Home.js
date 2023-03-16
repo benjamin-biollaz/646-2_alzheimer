3d041cf67b8b645718601739fb5dce2c78b2509b
Fabio <fabio.maiodealmeida@students.hevs.ch>
2023-03-16 13:21:01 +0100

    Unit Test Bug correction

--- src/Presentation/Components/Home.js
+++ src/Presentation/Components/Home.js
@@ -9,8 +9,9 @@ export default function Home({ resident }) {
 
   return (
     <div style={{ marginLeft: "10%", marginTop: "5%" }}>
-      <h1 data-testid="1">Hello {resident?.firstName}</h1>
-      <Link to="/infos">Information page</Link>
+      <h1 data-testid="1">Hello</h1>
+      <h1 data-testid="2">{resident?.firstName}</h1>
+      <button color="link" size="lg" onClick={event => window.location.href='infos/'}>Infos page</button>
     </div>
   );
 
