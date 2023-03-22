d364a63871427c6b487a320cfaf1eb1557172ce8
benjamin <benjamin.biollaz@students.hevs.ch>
2023-03-22 10:15:23 +0100

    preferences display

--- src/Presentation/Components/Preferences/PreferencesList.js
+++ src/Presentation/Components/Preferences/PreferencesList.js
@@ -9,6 +9,7 @@ import { TbFishOff } from "react-icons/tb";
 import { FaBath } from "react-icons/fa";
 import { GiShower } from "react-icons/gi";
 import { PreferenceDAO } from '../../../DAL/PreferenceDAO';
+import Preference from './Preference';
 
 function PreferencesList() {
 
@@ -21,43 +22,29 @@ function PreferencesList() {
     const getPreferencesList = async () => {
         const prefDAO = new PreferenceDAO();
         const pref = await prefDAO.getPreferencesByResidentId("HvrELV7MRnnJcV24ro1w");
-        console.log(pref);
         setPreferences(pref);
+    }
+
+    const renderFilteredPreferences = (category) => {
+        return (
+            <div className="infos_list">
+            {" "}
+            <h4 className="categories">{category}</h4>
+            <span className="infos_item">
+                {preferencesState?.filter((pf) => pf.category == category).map((p) =>
+                    <Preference preferenceDTO={p} key={p.label + p.iconName}></Preference>
+                )}
+            </span>
+        </div>
+        );
     }
 
     return (
         <div className="preferences">
             <h3 className="label">Préférences</h3>
-            <span>
-                {preferencesState?.map((p) => 
-                    <span key={p.label + p.iconName}>{p.iconName}: {p.label}, </span>    
-                )}
-            </span>
-            <div className="infos_list">
-                <h4 className="categories">Alimentation</h4>
-                <span className="infos_item">
-                    <div className="icon">
-                        <GiFullPizza />
-                    </div>
-                </span>
-                &nbsp; &nbsp;
-            </div>
-
-            <div className="infos_list">
-                {" "}
-                <h4 className="categories">Hygiène</h4>
-                <span className="infos_item">
-                    <div className="icon">
-                        <FaBath />
-                    </div>
-                </span>
-                &nbsp; &nbsp; &nbsp;
-                <span className="infos_item">
-                    <div className="icon">
-                        <GiShower />
-                    </div>
-                </span>
-            </div>
+            {renderFilteredPreferences("Alimentation")}
+            {renderFilteredPreferences("Hygiène")}
+            {renderFilteredPreferences("Sommeil")}
         </div>
     )
 }
