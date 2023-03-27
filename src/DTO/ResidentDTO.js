80017379fd57cdf11af01681be33819ee83ddadb
benjamin <benjamin.biollaz@students.hevs.ch>
2023-03-27 11:15:35 +0200

    belief DTO classes

--- src/DTO/ResidentDTO.js
+++ src/DTO/ResidentDTO.js
@@ -2,21 +2,26 @@
  * ResidentDTO contains the resident properties and a converter from and to Firestore.
  */
 class ResidentDTO {
-    constructor(firstName, lastName, birthDate) {
+    constructor(firstName, lastName, birthDate, religionId, valueIds, practiceIds) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthDate = birthDate
+        this.religionId = religionId;
+        this.valueIds = valueIds;
+        this.practiceIds = practiceIds;
     }
 
     toString() {
-        return this.firstName + " " + this.lastName + " " + this.birthDate;
+        return this.firstName + " " + this.lastName + " " + this.birthDate + " " + this.religionId
+            + " " + this.valueIds + " " + this.practiceIds;
     }
 }
 
 const residentConverter = {
     toFirestore(resident) {
         return {
-            firstName: resident.firstName, lastName: resident.lastName, birthDate: resident.birthDate
+            firstName: resident.firstName, lastName: resident.lastName, birthDate: resident.birthDate,
+            religionId: resident.religionId, valueIds: resident.valueIds, practiceIds: resident.practiceIds
         };
     },
     fromFirestore(
@@ -24,7 +29,8 @@ const residentConverter = {
         options
     ) {
         const data = snapshot.data(options);
-        return new ResidentDTO(data.firstName, data.lastName, data.birthDate);
+        return new ResidentDTO(data.firstName, data.lastName, data.birthDate, data.religionId, data.valueIds,
+            data.practiceIds);
     }
 };
 export { residentConverter };
