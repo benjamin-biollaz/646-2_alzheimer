2070f1cc4dcc063aa83f49a9c202ccd2772717ad
Benjamin Morel <benjamin.morel@students.hevs.ch>
2023-03-27 10:19:15 +0200

    Adding Login (back + front), logout function, restrict access

--- src/Presentation/Components/Information.js
+++ src/Presentation/Components/Information.js
@@ -1,27 +1,14 @@
-import React from "react";
+import React from 'react';
 import Navbar from "./Navbar";
 import "../CSS/Information.css";
 import "../fonts/LexendDeca.ttf";
 import { GiMusicalNotes } from "react-icons/gi";
-import { GiFullPizza } from "react-icons/gi";
-import { GiNoodles } from "react-icons/gi";
-import { TbCarrotOff } from "react-icons/tb";
-import { TbPizzaOff } from "react-icons/tb";
-import { TbFishOff } from "react-icons/tb";
-import { FaBath } from "react-icons/fa";
-import { GiShower } from "react-icons/gi";
-import { GiNightSleep } from "react-icons/gi";
-import { BsFillSunriseFill, BsSunriseFill } from "react-icons/bs";
-import { TfiTimer } from "react-icons/tfi";
-import { GiMeal } from "react-icons/gi";
 import { TimelineWidget } from "./Timeline/Timeline";
-import { useParams } from "react-router";
 import PreferencesList from "./Preferences/PreferencesList";
 import PassionsList from "./Passions/PassionsList";
 import PersonalInfos from "./PersonalInfos";
 import HabitsList from "./Habits/HabitsList";
 import BeliefsList from "./Beliefs/BeliefsList";
-import SectionHeader from "./SectionHeader";
 
 function Information() {
   function MusicNoteIcon() {
