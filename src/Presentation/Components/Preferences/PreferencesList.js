bf203d815df76dbc4d92ce70f967ddea8d1b1315
etdnp <etdn.prod@gmail.com>
2023-04-18 09:38:32 +0200

    information page css corrected

--- src/Presentation/Components/Preferences/PreferencesList.js
+++ src/Presentation/Components/Preferences/PreferencesList.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "../../CSS/Information.css";
 import { PreferenceDAO } from "../../../DAL/PreferenceDAO";
-import EditButton from "../Buttons/EditButton";
-import Preference from "./Preference";
+import { iconSets } from "../IconPopup/IconPicker";
 import NestedHeader from "../NestedHeader";
 import PrefPopUPContent from "./PrefPopUPContent";
 
@@ -32,6 +31,14 @@ function PreferencesList() {
     setPrefHygiene(pref.filter((p) => p.preferenceDTO.category == "Hygiène"));
   };
 
+  const getIconByName = (iconName) => {
+    for (const category of iconSets) {
+      for (const icons of category.icons) {
+        if (icons.name === iconName) return icons;
+      }
+    }
+  };
+
   // renders a list of preferences as readonly
   const renderPreferences = (category, preferencesState) => {
     return (
@@ -40,10 +47,12 @@ function PreferencesList() {
         <h4 className="categories">{category}</h4>
         <span className="infos_item">
           {preferencesState?.map((p) => (
-            <Preference
-              prefWithId={p}
-              key={p.preferenceDTO.label + p.preferenceDTO.iconName}
-            ></Preference>
+            <span>
+              <span className="icon-wrapper">
+                {getIconByName(p.preferenceDTO.iconName).icon}
+              </span>
+              {p.preferenceDTO.label}
+            </span>
           ))}
         </span>
       </div>
