68ea14c29ef7a36b75f6f162b733262b99437df3
Benjamin Morel <benjamin.morel@students.hevs.ch>
2023-04-20 12:46:52 +0200

    Adding logo

--- src/DTO/PreferenceWithId.js
+++ src/DTO/PreferenceWithId.js
@@ -1,3 +1,7 @@
+/**
+ * PreferenceWithId contains both the Firestore document id and the DTO object that can be sent to the database.
+ * This document id is needed in the program as an event doesn't have a unique identifier.
+ */
 class PreferenceWithId {
     constructor(id, preferenceDTO) {
         this.id = id;
@@ -5,4 +9,4 @@ class PreferenceWithId {
     }
 }
 
-export {PreferenceWithId}
+export { PreferenceWithId }
